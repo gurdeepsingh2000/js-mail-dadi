62e4d7f7cb9a807d536e4411d04ad818fdf60701
gurdeepsingh2000 <gurdeepsingh20000329@gmail.com>
2021-05-31 15:22:53 +0200

    esercizio mail finito

---
+++ mail/js/my-script.js
@@ -0,0 +1,22 @@
+var addresses = ["Marco@gmail.com" , "Luigi@gmail.com" , "Valerio@gmail.com" , "Alex@gmail.com"]
+var mail_address = prompt('Inserisci qui la tua mail');
+
+
+var output = document.getElementById('mail');
+
+
+var check = false; 
+
+
+for (var i = 0; i < addresses.length; i++) {
+    if (addresses[i] === mail_address) {
+        check = true;
+    }
+}
+
+if (check) {
+    output.innerHTML += "Ho trovato la tua mail!";
+} else {
+    output.innerHTML += "Non ho trovato la tua mail!";
+}
+
