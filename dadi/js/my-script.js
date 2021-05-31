53256d6881e251d6155016269bdb8ca14e653b07
gurdeepsingh2000 <gurdeepsingh20000329@gmail.com>
2021-05-31 17:51:33 +0200

    update esercizio dadi

--- dadi/js/my-script.js
+++ dadi/js/my-script.js
@@ -1,10 +1,14 @@
 var player_1 = parseInt(Math.floor(Math.random()* 6 + 1));
 var player_2 = parseInt(Math.floor(Math.random()* 6 + 1));
+var adv_1 = parseInt(player_1) - parseInt(player_2);
+var adv_2 = parseInt(player_2) - parseInt(player_1);
 
 var output = document.getElementById('dice');
 
 if (player_1 > player_2){
-    output.innerHTML += "Giocatore 1 Ha vinto!";
+    output.innerHTML += "Giocatore 1 Ha vinto con lo scarto di " + adv_1 + ' Punti !';
+}else if (player_1 < player_2){
+    output.innerHTML += "Giocatore 2 Ha vinto con lo scarto di " + adv_2 + ' Punti !';
 }else{
-    output.innerHTML += "Giocatore 2 Ha vinto!";
+    output.innerHTML += "E un pareggio!";
 }
